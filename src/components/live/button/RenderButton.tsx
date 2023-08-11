59d07c57b410425a2a04a773b116444c31a33b42
anisg <anisoupe@gmail.com>
2023-08-11 19:07:09 +0200

    [LiveDesign] Updated 2 components

--- src/components/live/button/RenderButton.tsx
+++ src/components/live/button/RenderButton.tsx
@@ -1,27 +1,23 @@
+
 // ~ auto-generated - do not edit ~
 import React from 'react';
-import { combine, cls, Div, DivProps, Span, SpanProps, Svg, SvgProps, Img, ImgProps } from '../lib';
+import { cls, HtmlButton, ElementProps, Span, SpanProps } from '../lib';
 
 
 export type RenderButtonProps = {
-  frames?: {
-    ["button"]?: DivProps
-    ["title"]?: SpanProps
+    frames?: {
+    ["button"]?: ElementProps<"button">
+    ["clickMe"]?: SpanProps
 
   }
-  ;
+;
 }
 
 export function RenderButton(props: RenderButtonProps) {
-  const { frames = {} } = props;
-  return (
-    <Div {...frames["button"]} className={cls(frames, "button", `
-            gap-0 flex-col relative bg-[#295cd7] 
-            opacity-100 px-3 py-1.5
-            flex`)}>
-      <Span {...frames["title"]} className={cls(frames, "title", `
-            font-['Inter'] text-lg font-semibold
-            text-white text-left relative`)}>click me</Span>
-    </Div>
-  );
+    const { frames = {} } = props;
+    return (
+        <HtmlButton {...frames["button"]} className={cls(frames,"button", `gap-0 flex-col relative border rounded bg-[#2758ce] opacity-100 px-3 py-1.5 border-[#2758ce] flex`)}>
+  <Span {...frames["clickMe"]} className={cls(frames,"clickMe", `font-['Inter'] text-lg font-semibold text-white text-left relative`)}>click me</Span>
+</HtmlButton>
+    );
 }
