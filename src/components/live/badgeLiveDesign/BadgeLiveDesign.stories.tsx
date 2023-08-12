7bfee8fe4c163d9b4530fe2d13eb76f2fd9930d1
anisg <anisoupe@gmail.com>
2023-08-12 12:47:25 +0200

    [LiveDesign] Updated 3 components

---
+++ src/components/live/badgeLiveDesign/BadgeLiveDesign.stories.tsx
@@ -0,0 +1,16 @@
+
+// ~ auto-generated - do not edit ~
+import React from 'react';
+import { cls, LIB_IMPORTS } from '../lib';
+IMPORTS
+
+export type RenderBadgeLiveDesignProps = {
+    frames?: FRAMES_PROPS;
+}
+
+export function RenderBadgeLiveDesign(props: RenderBadgeLiveDesignProps) {
+    const { frames = {} } = props;
+    return (
+        OUTPUT
+    );
+}
---
+++ src/components/live/button/Button.stories.tsx
@@ -0,0 +1,16 @@
+
+// ~ auto-generated - do not edit ~
+import React from 'react';
+import { cls, LIB_IMPORTS } from '../lib';
+IMPORTS
+
+export type RenderButtonProps = {
+    frames?: FRAMES_PROPS;
+}
+
+export function RenderButton(props: RenderButtonProps) {
+    const { frames = {} } = props;
+    return (
+        OUTPUT
+    );
+}
